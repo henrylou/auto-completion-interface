9255171cae9de9acb5470158cc082b87a5c8bf25
Hengyu Lou <hylou34@gmail.com>
2018-10-15 16:52:52 -0700

    Add README

---
+++ README.md
@@ -0,0 +1,7 @@
+# Autocomplete Interface
+
+The autocomplete user interface predicts the rest of words a user is typing.
+
+## Demo
+
+The demo website is **https://auto-complete-interface.herokuapp.com**
--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <title>Autocomplete using PHP/Ajax and jQuery</title>
+  <title>Autocomplete</title>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
   <style>
