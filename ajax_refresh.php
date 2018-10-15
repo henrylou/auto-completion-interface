7051f46f37b7873be654a04899941ab41f92610e
Hengyu Lou <hylou34@gmail.com>
2018-10-14 23:07:03 -0700

    Set up database for production

--- ajax_refresh.php
+++ ajax_refresh.php
@@ -1,37 +1,22 @@
 <?php
 
-// PDO connect *********
-// define ('DBUSER', 'root');
-// define ('DBPASS','root');
-// define ('DBNAME','test');
-
-$dbstr = getenv('CLEARDB_DATABASE_URL');
-$dbstr = substr("$dbstr", 8);
-$dbstrarruser = explode(":", $dbstr);
-
-$dbstrarrrecon = explode("?", $dbstrarrhost[1]);
-$dbstrarrport = explode("/", $dbstrarrrecon[0]);
-$dbpassword = $dbstrarrhost[0];
-$dbhost = $dbstrarrport[0];
-$dbport = $dbstrarrport[0];
-$dbuser = $dbstrarruser[0];
-$dbname = $dbstrarrport[1];
-unset($dbstrarrrecon);
-unset($dbstrarrport);
-unset($dbstrarruser);
-unset($dbstrarrhost);
-unset($dbstr);
-
-$server = 'mysql:host=' . $dbhost . ';dbname=' . $dbname;
-$db = new PDO($server, $dbuser, $dbpassword);
-$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-
-// function connect() {
-//     return new PDO('mysql:host=localhost;dbname=test;port=8889', 'root', 'root', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
-// }
-
 function connect() {
-    return new PDO($server, $dbuser, $dbpassword, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+	$url = getenv('CLEARDB_DATABASE_URL');
+	$str = substr("$url", 8);
+
+	$user = explode(":", $str)[0];
+	$str = explode(":", $str)[1];
+
+	$password = explode("@", $str)[0];
+	$str = explode("@", $str)[1];
+
+	$host = explode("/", $str)[0];
+	$str = explode("/", $str)[1];
+
+	$db = explode("?", $str)[0];
+
+	$server = "mysql:host=".$host.";dbname=".$db;
+    return new PDO($server, $user, $password, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
 }
 
 $pdo = connect();
@@ -41,6 +26,7 @@ $query = $pdo->prepare($sql);
 $query->bindParam(':keyword', $keyword, PDO::PARAM_STR);
 $query->execute();
 $list = $query->fetchAll();
+
 foreach ($list as $rs) {
 	// put in bold the written text
 	// $country_name = str_replace($_POST['keyword'], '<b>'.$_POST['keyword'].'</b>', $rs['following_word']);
